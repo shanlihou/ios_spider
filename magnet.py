33229b6ae7987ba16b30441d46c8cb26a3746871
shanlihou <shanlihou@gmail.com>
2020-09-05 18:00:02 +0800

    up

--- magnet.py
+++ magnet.py
@@ -5,7 +5,8 @@ import urllib
 import re
 import sys
 import gzip
-URIBASE = 'btsow.casa'
+#URIBASE = 'btsow.casa'
+URIBASE = 'btsow.website'
 HEADERS = {'Host': URIBASE, 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3053.3 Safari/537.36',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 'Connection': 'keep-alive', 'Accept-Encoding': 'gzip, deflate, sdch, br', 'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6'}
 
@@ -32,16 +33,18 @@ def get_content(urlPath, is_save=False):
 
 class BtOne(object):
     def __init__(self, url, size, name):
-        content = get_content(url, True)
+        # content = get_content(url, True)
 #         content = get_save('one.html')
-        self.mag = self.parse_mag(content)
+        self.url = url
+        #self.mag = self.parse_mag(content)
         self.size = size
         self.name = name
 
     def __str__(self):
         return '{},{}'.format(self.mag, self.size)
 
-    def parse_mag(self, content):
+    @staticmethod
+    def parse_mag(content):
         pat = re.compile(r'(magnet:\?xt=urn:btih:[^"><]+)["<>]')
         ret = pat.findall(content)
         return ret[0]
@@ -66,10 +69,18 @@ def parse_content(content):
             a = ret.select('a')[0]
             name = a.select('div')[0]
             name = rec_content(name)
-            n_div = a.find_next_siblings('div')[0]
-            yield BtOne(a['href'], n_div.string, name)
+            size = a.find_next_siblings('div')[0].string
+            url = a['href']
+            hash_code = url.split('/')[-1]
+            mag = 'magnet:?xt=urn:btih:{}'.format(hash_code)
+            yield BtOne(mag, size, name)
 #     for url in ret:
 #         #bo = BtOne(url)
+
+def get_mag_by_hash(hash_code):
+    url = 'https://{}/magnet/detail/hash/{}'.format(URIBASE, hash_code)
+    content = get_content(url, True)
+    return BtOne.parse_mag(content)
 
 
 def getAllMagnet(code):
@@ -79,12 +90,16 @@ def getAllMagnet(code):
     url = 'https://{}/search/{}'.format(URIBASE, code)
     content = get_content(url, True)
     for bo in parse_content(content):
-        yield bo.mag, bo.size, bo.name
+        yield bo.url, bo.size, bo.name
 
 
 if __name__ == '__main__':
-    #     parse_content(get_save())
+    #for a in parse_content(get_save('one.html')):
+    #    print(a.url)
+    mag = get_mag_by_hash('D71277C8188FF54F11B3D795BCE1B3736A499EE9')
+    print(mag)
     #     bo = BtOne(1)
-    getAllMagnet('复仇者')
+    #for a, b, c in getAllMagnet('复仇者'):
+    #    print(a, b, c)
 #     if (len(sys.argv) == 2):
 #         getAllMagnet(sys.argv[1])
--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 kill -9 `ps -aux|grep rasp_server |grep -v grep|awk '{print $2}'`
-rm nohup.out
+rm rasp.out
 nohup python3 rasp_server.py >>rasp.out 2>&1 &
